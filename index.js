04658c93c55f8c6a57c92a43369eabd35c17ec86
sgofferj <stefan@gofferje.net>
2023-03-31 16:05:30 +0300

    Corrections

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const fetch = require('node-fetch');
 const url = process.env.REMOTE_SERVER_URL
 const sslCert = process.env.REMOTE_SSL_SERVER_CERTIFICATE
 const sslKey = process.env.REMOTE_SSL_SERVER_KEY
-const intervalSecs = (typeof process.env.UPDATE_INTERVAL !== 'undefined') ? process.env.UPDATE_INTERVAL : 2.1;
+const intervalSecs = (typeof process.env.UPDATE_INTERVAL !== 'undefined') ? process.env.UPDATE_INTERVAL : 2;
 if (intervalSecs < 2) intervalSecs = 2;
 const logCot = (typeof process.env.LOGCOT !== 'undefined') ? process.env.LOGCOT : false;
 const getMil = (typeof process.env.GETMIL !== 'undefined') ? process.env.GETMIL : false;
